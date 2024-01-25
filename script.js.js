758d6fefe9177cb8f3550d0aa996636304f302c7
Paolo Gomez <peweegardner248@gmail.com>
2024-01-25 07:40:23 -0800

    update

---
+++ script.js.js
@@ -0,0 +1,15 @@
+// Import Leaflet library
+import L from 'leaflet';
+
+// Initialize the map
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add a tile layer (you can use different map providers)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Example marker
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A sample marker')
+    .openPopup();
